e7accf66638c083c09207ad3bcc562ad74218722
Elitex <jain.yashu2211@gmail.com>
2022-01-09 07:13:19 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const Topgg = require('@top-gg/sdk')
+const fetch = require(`node-fetch`)
+const app = express()
+
+const webhook = new Topgg.Webhook('<webhook-auth>') //It can be anything
+
+app.post('/vote', webhook.listener(vote => { //ending url
+    let value = JSON.stringify({
+        content : `<@${vote.user}> voted for Raiden`
+    })
+    fetch(`<discord-webhook-url>`, {
+        method : 'POST',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: value
+    }).catch(e => console.log(`Error occured while parsing webhook: `+ e))
+  
+}))
+
+app.listen(0101) //port of the console panel
+console.log(`Top.gg webhook is ready to go.`)
